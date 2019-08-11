9344d5508794fd37a9de422d739c2dc8ffc2114d
Bad Stark <bad-stark@yandex.ru>
2019-08-11 22:55:36 +0400

    1

--- picUps.php
+++ picUps.php
@@ -1,5 +1,8 @@
 <?php
-if (!empty($_FILES) || isset($_FILES['myimage']) || 0 == $_FILES['myimage']['error']){
+if (!empty($_FILES) &&
+    isset($_FILES['myimage']) && 0 == $_FILES['myimage']['error'] && $_FILES['myimage']['type'] == 'image/png'){
     $name = $_FILES['myimage']['name'];
     move_uploaded_file($_FILES['myimage']['tmp_name'], __DIR__ . '/pics/' . $name );
+} else {
+    echo 'Ошибка';
 }
